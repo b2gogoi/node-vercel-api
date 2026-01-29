408a62844542fca77967e1da227c332e9bac12e7
Bhaskar Gogoi <bhaskar@foodieverseindia.com>
2026-01-29 05:22:02 +0000

    vercel json

--- api/users.js
+++ api/users.js
@@ -1,8 +1,11 @@
+import cors from 'cors';
+
 export default function handler(req, res) {
     const users = [
         { id: 1, name: 'Alice'},
-        { id: 2, name: 'Bob'}
+        { id: 2, name: 'Bob'},
+        { id: 3, name: 'Bhaskar'}
     ];
-
+    res.setHeader('Access-Control-Allow-Origin', '*')
     res.status(200).json(users);
 }
